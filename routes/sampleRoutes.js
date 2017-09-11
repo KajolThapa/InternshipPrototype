ab69bb6f34e95a7b56ee99bcfec2d4fe480bc2c0
patrick.hauke <patrick.hauke@outlook.com>
2017-09-11 10:49:22 -0400

    minor addition to front end

--- routes/sampleRoutes.js
+++ routes/sampleRoutes.js
@@ -1,10 +1,24 @@
 var express = require('express'),
-    router = express.Router();
+    app = express(),
+    router = express.Router(),
+    bodyParser = require('body-parser');
+
+// Use bodyParser to parse incoming requests to server
+// https://github.com/expressjs/body-parser
+app.use(bodyParser.json()); // parses only json objects
+app.use(bodyParser.urlencoded({
+  extended:true // allow foor rich json objects and arrays
+}));
 
 router.get('/form', function(req, res){
   return res.render('layout');
 });
 
+router.post('/setDepartment', function(req, res){
+  var department = req.body.sample;
+  // Need to resolve accessing datalist object
+  console.log(department);
+});
 
 
 module.exports = router;
