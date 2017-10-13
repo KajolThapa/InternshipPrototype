57e6f17c9509821612b9ba80ce3ff2399fd17e4a
patrick.hauke <patrick.hauke@outlook.com>
2017-10-13 00:19:22 -0400

    i should have been asleep... Buttons now submit survey id and other meta data to server. New page not rendering, although it should will be easy to fix

--- routes/routes.js
+++ routes/routes.js
@@ -12,7 +12,7 @@ router.get('/', function(req, res){
     });
 });
 
-router.post('/getsurvey', function(req, res){
+router.post('/getsurveylist', function(req, res){
     var deptID = req.body.deptID,
         email = req.body.email,
         deptName = req.body.deptName;
@@ -106,5 +106,55 @@ router.post('/getsurvey', function(req, res){
     // console.log('getting email :: '+ email);
     // console.log('getting dept id :: '+ deptId);
 })
+
+router.post('/survey', function(req, res){
+    console.log(req.body);
+    // Take survey code, pass to query. Render ejs along with query response
+    return res.render('forms/survey', {
+        title: "saml",
+        surveyData :{
+            surveyId: "sample-survey",
+            surveyName: "sample-name",
+            questionKey: [
+                {
+                    id: 0,
+                    question: "What is soap?",
+                    questionType: 1, // single input, multiple choice (checkbox)
+                    answerKey: [
+                        { ans: "soapy" },
+                        { ans: "super soap" },
+                        { ans : "sub-soap" }
+                    ]
+                }, {
+                    id: 1,
+                    question: "What does cow say?",
+                    questionType: 2, // Allow multiple inputs (checkbox)
+                    answerKey: [
+                        { ans: "MOO!" },
+                        { ans: "BAAHH!!" },
+                        { ans : "BUUHHH" },
+                        { ans : "ARF!"}
+                    ]   
+                }, {
+                    id: 2,
+                    question: "On a scale of 1-5, How much do you like pizza?",
+                    questionType: 3, // single input, rating scale (radio)
+                    answerKey: [
+                        { ans: "1" },
+                        { ans: "2" },
+                        { ans: "3" },
+                        { ans: "4" },
+                        { ans: "6" }
+                    ]
+                }, {
+                    id: 3,
+                    question: "What is your favorite type of floof?",
+                    questionType: 0, // user string entry
+                    answerKey: []
+                }
+            ]
+        }
+    })
+});
 
 module.exports = router;
