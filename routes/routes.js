4617d2b7b815852ad6535f51e51a8895675b094f
patrick.hauke <patrick.hauke@outlook.com>
2017-10-16 01:07:04 -0400

    pulled test data from routes and am now importing from external json file

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,7 @@ var express = require('express'),
 // Static departments for now
 var departmentList = require('../departmentList.json');
 
+var surveyQuestions = require('../surveyData.json');
 router.get('/', function(req, res){
     // console.log(departmentList.departments);
     return res.render('forms/launch', {
@@ -116,85 +117,15 @@ router.post('/survey', function(req, res){
 // router.get('/survey/:email/:deptid/:surveyId', function(req, res){
     console.log(req.body);
     console.log('build survey');
-    res.render('forms/survey'
-    , {
+    res.render('forms/survey', {
         email: req.body.email,
         deptId: req.body.deptID,
-        surveyData: {
-            surveyName: "sample-name",
-            questionKey: [
-                {
-                    id: 0,
-                    question: "What is soap?",
-                    questionType: 1, // single input, multiple choice (checkbox)
-                    answerKey: [
-                        { ans: "soapy" },
-                        { ans: "super soap" },
-                        { ans : "sub-soap" }
-                    ]
-                }, {
-                    id: 1,
-                    question: "Floof?",
-                    questionType: 2, // single input, multiple choice (checkbox)
-                    answerKey: [
-                        { ans: "woof" },
-                        { ans: "Moo?" },
-                        { ans : "Chup" }
-                    ]
-                }
-            ]
-        }
+        surveyData: surveyQuestions
     });
-    // Take survey code, pass to query. Render ejs along with query response
-    // return res.render('forms/survey', {
-    //     title: "saml",
-    //     surveyData :{
-    //         surveyId: "sample-survey",
-    //         surveyName: "sample-name",
-    //         questionKey: [
-    //             {
-    //                 id: 0,
-    //                 question: "What is soap?",
-    //                 questionType: 1, // single input, multiple choice (checkbox)
-    //                 answerKey: [
-    //                     { ans: "soapy" },
-    //                     { ans: "super soap" },chec
-    //                     { ans : "sub-soap" }
-    //                 ]
-    //             }, {
-    //                 id: 1,
-    //                 question: "What does cow say?",
-    //                 questionType: 2, // Allow multiple inputs (checkbox)
-    //                 answerKey: [
-    //                     { ans: "MOO!" },
-    //                     { ans: "BAAHH!!" },
-    //                     { ans : "BUUHHH" },
-    //                     { ans : "ARF!"}
-    //                 ]   
-    //             }, {
-    //                 id: 2,
-    //                 question: "On a scale of 1-5, How much do you like pizza?",
-    //                 questionType: 3, // single input, rating scale (radio)
-    //                 answerKey: [
-    //                     { ans: "1" },
-    //                     { ans: "2" },
-    //                     { ans: "3" },
-    //                     { ans: "4" },
-    //                     { ans: "6" }
-    //                 ]
-    //             }, {
-    //                 id: 3,
-    //                 question: "What is your favorite type of floof?",
-    //                 questionType: 0, // user string entry
-    //                 answerKey: []
-    //             }
-    //         ]
-    //     }
-    // })
 });
 
 router.post('/submit', function(req, res){
     console.log(req.body);
-    res.send('SUBMIT!');
+    res.send('Data submitted to server');
 })
 module.exports = router;
