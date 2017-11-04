814e1792c375d9922dc39084b614d1e287df99c2
josephkem <josephkemoy@gmail.com>
2017-11-04 00:24:10 -0400

    Data layout
    I added a field for question numbers  to have multiple digits since questions can have multiple parts.

--- airdatasample.sql
+++ airdatasample.sql
@@ -1,50 +1,18 @@
-
-SHOW con_name;
-SHOW user;
-
 create table Survey(
-survey_id int,
-survey_name varchar(20),
-PRIMARY KEY(survey_id)
-);
+survey_id int constraint surv_pk PRIMARY KEY,
+survey_name varchar(20));
 
 create table Department(
-dept_id int,
-dept_name varchar(20),
-PRIMARY KEY(dept_id),
-FOREIGN KEY(dept_id) REFERENCES Survey(survey_id) 
-);
+dept_id int constraint dept_pk PRIMARY KEY,
+dept_name varchar(50));
 
 create table Questions(
-
-
-quest_id int,
+quest_id int constraint quest_fk references Survey(survey_id), 
+quest_num int constraint q_numpk PRIMARY KEY,
 quest_type varchar(10),
-quest_name varchar(20),
-PRIMARY KEY(quest_id),
-FOREIGN KEY(quest_id) REFERENCES Department(dept_id)
+quest_name varchar(50));
 
-);
 
 create table Answers(
-Ans_id int,
-Ans_type varchar(20),
-Ans varchar(50),
-PRIMARY KEY(Ans_id)
-);
-
-insert into Questions values(1,'txtbx','Total men applied');
-insert into Questions values(2,'txtbx','Total women applied');
-insert into Questions values(3,'txtbx','Total applications');
-insert into Questions values(4,'txtbx','Total men admitted');
-insert into Questions values(5,'txtbx','Total women admitted');
-insert into Questions values(6,'txtbx','Total admitted');
-
-insert into Department values(1,'Admissions');
-insert into Department values(2,'Admissions');
-insert into Department values(3,'Admissions');
-insert into Department values(4,'Admissions');
-insert into Department values(5,'Admissions');
-insert into Department values(6,'Admissions');
-
-Select * from Questions;
+ans_num int constraint ans_fk references Questions(quest_num),
+ans_name varchar(30));
